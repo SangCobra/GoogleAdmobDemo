4ae47f03bf993db1688a4eb4b5801a5fc5c3fe0b
Admin <sangtrinh04032002@gmail.com>
2022-09-17 10:11:00 +0700

    apply Institial and Reward Ads Mob

--- app/src/main/java/com/example/googleadsmobtesting/AdsActivity.java
+++ app/src/main/java/com/example/googleadsmobtesting/AdsActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -16,13 +17,21 @@ import com.google.android.gms.ads.AdRequest;
 import com.google.android.gms.ads.FullScreenContentCallback;
 import com.google.android.gms.ads.LoadAdError;
 import com.google.android.gms.ads.MobileAds;
+import com.google.android.gms.ads.OnUserEarnedRewardListener;
 import com.google.android.gms.ads.appopen.AppOpenAd;
 import com.google.android.gms.ads.initialization.InitializationStatus;
 import com.google.android.gms.ads.initialization.OnInitializationCompleteListener;
+import com.google.android.gms.ads.interstitial.InterstitialAd;
+import com.google.android.gms.ads.interstitial.InterstitialAdLoadCallback;
+import com.google.android.gms.ads.rewarded.RewardItem;
+import com.google.android.gms.ads.rewarded.RewardedAd;
+import com.google.android.gms.ads.rewarded.RewardedAdLoadCallback;
 
 public class AdsActivity extends Activity {
+    private static final String TAG = "AdsActivity";
     private AppOpenAdManager appOpenAdManager;
     private ActMainBinding binding;
+    private RewardedAd mRewardedAd;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -31,17 +40,84 @@ public class AdsActivity extends Activity {
         binding = ActMainBinding.inflate(LayoutInflater.from(this));
         setContentView(binding.getRoot());
 
+        initAdsMob();
+
+        appOpenAdManager = new AppOpenAdManager();
+        showAppOpenAds();
+        
+        showAdBanner();
+
+        loadAdReward();
+        
+        initEvent();
+
+        
+    }
+
+    private void loadAdReward() {
+        AdRequest adRequest = new AdRequest.Builder().build();
+        RewardedAd.load(this, "ca-app-pub-3940256099942544/5224354917",
+                adRequest, new RewardedAdLoadCallback() {
+                    @Override
+                    public void onAdFailedToLoad(@NonNull LoadAdError loadAdError) {
+
+                    }
+
+                    @Override
+                    public void onAdLoaded(@NonNull RewardedAd rewardedAd) {
+                        mRewardedAd = rewardedAd;
+                        onAdRewardLoaded();
+                    }
+                });
+    }
+
+    private void onAdRewardLoaded(){
+        mRewardedAd.setFullScreenContentCallback(new FullScreenContentCallback() {
+
+            @Override
+            public void onAdClicked() {
+                // Called when a click is recorded for an ad.
+                Log.d(TAG, "Ad was clicked.");
+            }
+
+            @Override
+            public void onAdDismissedFullScreenContent() {
+                // Called when ad is dismissed.
+                // Set the ad reference to null so you don't show the ad a second time.
+                Log.d(TAG, "Ad dismissed fullscreen content.");
+            }
+
+            @Override
+            public void onAdFailedToShowFullScreenContent(AdError adError) {
+                // Called when ad fails to show.
+                Log.e(TAG, "Ad failed to show fullscreen content.");
+                Toast.makeText(AdsActivity.this, adError.getMessage(),Toast.LENGTH_LONG).show();
+            }
+
+            @Override
+            public void onAdImpression() {
+                // Called when an impression is recorded for an ad.
+                Log.d(TAG, "Ad recorded an impression.");
+            }
+
+            @Override
+            public void onAdShowedFullScreenContent() {
+                // Called when ad is shown.
+                Log.d(TAG, "Ad showed fullscreen content.");
+            }
+        });
+    }
+
+    private void initAdsMob() {
         MobileAds.initialize(
                 this,
                 initializationStatus -> {
                 });
-        appOpenAdManager = new AppOpenAdManager();
+    }
 
-        showAppOpenAds();
+    private void showAdBanner() {
         AdRequest adRequest = new AdRequest.Builder().build();
         binding.adView.loadAd(adRequest);
-
-        initEvent();
     }
 
     private void initEvent() {
@@ -49,6 +125,28 @@ public class AdsActivity extends Activity {
             @Override
             public void onClick(View view) {
                 startActivity(new Intent(AdsActivity.this, ShowNativeAdsAct.class));
+            }
+        });
+
+        binding.reward.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (mRewardedAd != null) {
+                    Activity activityContext = AdsActivity.this;
+                    mRewardedAd.show(activityContext, new OnUserEarnedRewardListener() {
+                        @Override
+                        public void onUserEarnedReward(@NonNull RewardItem rewardItem) {
+                            // Handle the reward.
+                            Log.d(TAG, "The user earned the reward.");
+                            int rewardAmount = rewardItem.getAmount();
+                            String rewardType = rewardItem.getType();
+                            loadAdReward();
+                        }
+
+                    });
+                } else {
+                    Log.d(TAG, "The rewarded ad wasn't ready yet.");
+                }
             }
         });
     }
--- app/src/main/java/com/example/googleadsmobtesting/ShowNativeAdsAct.java
+++ app/src/main/java/com/example/googleadsmobtesting/ShowNativeAdsAct.java
@@ -4,42 +4,105 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
-import android.widget.LinearLayout;
-import android.widget.TextView;
+import android.widget.FrameLayout;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
-import com.example.googleadsmobtesting.databinding.ActNativeAdBinding;
 import com.example.googleadsmobtesting.databinding.LayoutNativeAdsBinding;
+import com.google.android.gms.ads.AdError;
 import com.google.android.gms.ads.AdListener;
 import com.google.android.gms.ads.AdLoader;
 import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.FullScreenContentCallback;
 import com.google.android.gms.ads.LoadAdError;
+import com.google.android.gms.ads.MediaAspectRatio;
+import com.google.android.gms.ads.interstitial.InterstitialAd;
+import com.google.android.gms.ads.interstitial.InterstitialAdLoadCallback;
 import com.google.android.gms.ads.nativead.NativeAd;
 import com.google.android.gms.ads.nativead.NativeAdOptions;
-import com.google.android.gms.ads.nativead.NativeAdView;
+
+import java.util.Objects;
 
 public class ShowNativeAdsAct extends Activity {
     private static final String AD_UNIT_ID = "ca-app-pub-3940256099942544/2247696110";
     private static final String TAG = "ShowNativeAdsAct";
 
     private AdLoader adLoader;
+    private InterstitialAd mInterstitialAd;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.act_native_ad);
 
+        showInterstitialAd();
+        showNativeAdsBanner();
+    }
+
+    private void showInterstitialAd() {
+        AdRequest adRequest = new AdRequest.Builder().build();
+
+        InterstitialAd.load(this,"ca-app-pub-3940256099942544/1033173712", adRequest,
+                new InterstitialAdLoadCallback() {
+                    @Override
+                    public void onAdLoaded(@NonNull InterstitialAd interstitialAd) {
+                        mInterstitialAd = interstitialAd;
+                        onInterstitialAdCreated();
+                    }
+
+                    @Override
+                    public void onAdFailedToLoad(@NonNull LoadAdError loadAdError) {
+                        mInterstitialAd = null;
+                    }
+                });
+
+
+    }
+
+    private void onInterstitialAdCreated(){
+        mInterstitialAd.setFullScreenContentCallback(new FullScreenContentCallback(){
+            @Override
+            public void onAdClicked() {
+                // Called when a click is recorded for an ad.
+            }
+
+            @Override
+            public void onAdDismissedFullScreenContent() {
+                mInterstitialAd = null;
+            }
+
+            @Override
+            public void onAdFailedToShowFullScreenContent(AdError adError) {
+                mInterstitialAd = null;
+            }
+
+            @Override
+            public void onAdImpression() {
+            }
+
+            @Override
+            public void onAdShowedFullScreenContent() {
+            }
+        });
+
+        if (mInterstitialAd != null) {
+            mInterstitialAd.show(ShowNativeAdsAct.this);
+        }
+    }
+
+    private void showNativeAdsBanner() {
         adLoader = new AdLoader.Builder(this, AD_UNIT_ID)
                 .forNativeAd(new NativeAd.OnNativeAdLoadedListener() {
                     @Override
                     public void onNativeAdLoaded(NativeAd nativeAd) {
                         // Show the ad.
-                        NativeAdView nativeAdsView = (NativeAdView) getLayoutInflater().inflate(R.layout.layout_native_ads, null);
+                        LayoutNativeAdsBinding nativeAdsView = LayoutNativeAdsBinding.inflate(LayoutInflater.from(ShowNativeAdsAct.this));
+                        populateNativeAdview(nativeAdsView, nativeAd);
 
-                        LinearLayout container = findViewById(R.id.container);
+                        FrameLayout container = findViewById(R.id.container);
                         container.removeAllViews();
-                        container.addView(nativeAdsView);
+                        container.addView(nativeAdsView.getRoot());
 
                         if (isDestroyed()) {
                             nativeAd.destroy();
@@ -62,5 +125,11 @@ public class ShowNativeAdsAct extends Activity {
                 .withNativeAdOptions(new NativeAdOptions.Builder().build())
                 .build();
         adLoader.loadAds(new AdRequest.Builder().build(), 3);
+    }
+
+    private void populateNativeAdview(LayoutNativeAdsBinding binding, NativeAd nativeAd) {
+        binding.adAppIcon.setImageDrawable(Objects.requireNonNull(nativeAd.getIcon()).getDrawable());
+        binding.adHeadline.setText(nativeAd.getHeadline());
+        binding.body.setText(nativeAd.getBody());
     }
 }
